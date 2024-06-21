b1f125bc60bfb57187fad99c641c607e3a3df8db
Rasyied11 <abdulrasyied182@gmail.com>
2024-06-21 19:36:21 +0800

    Project WeatherApp

---
+++ App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { BASE_URL, API_KEY } from './src/constant'
+import { View, StyleSheet, ActivityIndicator } from 'react-native'
+import WeatherSearch from './src/components/weatherSearch'
+import WeatherInfo from './src/components/weatherInfo'
+
+
+const App = () => {
+  const [weatherData, setWeatherData] = useState()
+  const searchWeather = (location) => {
+    axios
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data
+        data.visibility /= 1000
+        data.visibility = data.visibility.toFixed(2)
+        data.main.temp -= 273.15 // Konversi Kelvin ke Celcius
+        data.main.temp = data.main.temp.toFixed(2)
+        setWeatherData(data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  return (
+    <View style={styles.container}>
+      <WeatherSearch searchWeather={searchWeather} />
+      {weatherData && <WeatherInfo weatherData={weatherData} />}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+})
+
+export default App
---
+++ src/constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = 'f0effd6b64184f9eaa00817b408e4207';
